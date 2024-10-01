485c3eeb7f742a0111472ab3d8735f8a2d22c906
laucharip <supersangrejoven@gmai.com>
2024-10-01 18:38:52 -0300

    arrays

---
+++ navbar.js
@@ -0,0 +1,11 @@
+const categorias = ['remeras', 'pantalones', 'gorras'];
+
+const menu = `
+  <nav>
+    <ul>
+      ${categorias.map(categoria => `<li><a href="#">${categoria}</a></li>`).join('')}
+    </ul>
+  </nav>
+`;
+
+document.body.insertAdjacentHTML('afterbegin', menu);
